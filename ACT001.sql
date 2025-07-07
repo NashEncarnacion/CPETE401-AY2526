d4d115c5e19d31d5c2b78653007ea36f75ae2063
NashEncarnacion <111486543+NashEncarnacion@users.noreply.github.com>
2025-07-07 13:06:23 +0800

    updated ACT001

--- ACT001.sql
+++ ACT001.sql
@@ -1,52 +1,39 @@
 CREATE TABLE authors (
-    author_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
+    author_id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
     biography TEXT,
-    date_of_birth DATE,
-    PRIMARY KEY (author_id)
+    date_of_birth DATE
 );
 
 CREATE TABLE publishers (
-    publisher_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    country_of_origin VARCHAR(100),
-    PRIMARY KEY publisher_id
+    publisher_id INT,
+    name VARCHAR(150),
+    country_of_origin VARCHAR(60)
 );
 
 CREATE TABLE books (
-    book_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    title VARCHAR(100) NOT NULL,
-    isbn VARCHAR(20) NOT NULL,
+    book_id BIGINT,
+    title VARCHAR(255),
+    isbn VARCHAR(17),
     publication_date DATE,
-    price DECIMAL(10,2),
-    publisher_id INT UNSIGNED,
-    PRIMARY KEY (book_id),
-    FOREIGN KEY (publisher_id) REFERENCES publishers(publisher_id) ON DELETE SET NULL
-);
-
-CREATE TABLE book_authors (
-    book_id BIGINT UNSIGNED NOT NULL,
-    author_id INT UNSIGNED NOT NULL,
-    PRIMARY KEY (book_id, author_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE,
+    price DECIMAL(10,2)
 );
 
 CREATE TABLE patients (
     patient_id INT,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
     date_of_birth DATE,
-    phone_number VARCHAR(20),
+    phone_number VARCHAR(15),
     record_created DATE
 );
 
 CREATE TABLE doctors (
     doctor_id INT,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    specialty VARCHAR(20),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    specialty VARCHAR(50),
     year_licensed YEAR
 );
 
@@ -54,19 +41,18 @@ CREATE TABLE appointments (
     appointment_id INT,
     schedule DATETIME,
     reason TEXT,
-    status ENUM('Scheduled', 'Completed', 'Canceled', 'No-Show') DEFAULT 'Scheduled'
+    status ENUM('Scheduled', 'Completed', 'Canceled', 'No-Show')
 );
 
 CREATE TABLE departments (
     department_id INT,
-    name VARCHAR(50),
-    location VARCHAR(50)
+    name VARCHAR(100),
+    office_location VARCHAR(100)
 );
 
 CREATE TABLE courses (
     course_id INT,
     course_code VARCHAR(10),
-    course_title VARCHAR(50),
-    credits INT,
-    prerequisite_course_id INT
+    course_title VARCHAR(100),
+    credits INT
 );
